e6d2528f1a583c91b11ae839bf61b8515edf9685
Mattia Zago <dev@zagomattia.it>
2022-01-31 18:06:53 +0100

    Restored 2018

---
+++ 2018/assets/js/cookie.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#edeff5",
+      "text": "#838391"
+    },
+    "button": {
+      "background": "#4b81e8"
+    }
+  },
+  "showLink": false,
+  "theme": "classic",
+  "position": "bottom-right",
+  "content": {
+    "message": "This website uses Google Analytics with \nIP Anonymization to preserve your privacy while providing useful insights of the viewed pages.",
+    "dismiss": "That's fine",
+    "allow": "That's fine"
+  }
+})});
